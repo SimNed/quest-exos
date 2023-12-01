04cae43dab3dfdecae31b4f82a2ff2a791d9e928
SimNed <simon.nedjari@outlook.com>
2023-12-01 12:08:38 +0100

    add: dynamic data in PokemonCard

--- pokedex/src/components/PokemonCard.tsx
+++ pokedex/src/components/PokemonCard.tsx
@@ -1,8 +1,20 @@
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
 function PokemonCard() {
+    let pokemon = pokemonList[0]
     return(
         <figure>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur profil" />
-            <figcaption>Bulbasaur</figcaption>
+            {pokemon.imgSrc ? <img src={pokemon.imgSrc} alt={`${pokemon.name} profil`} /> : <p>???</p>}
+            <figcaption>{pokemon.name.toUpperCase()}</figcaption>
         </figure>
     )
 }
