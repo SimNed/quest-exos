d82cb865308034849772a7cb75ac902888464474
SimNed <simon.nedjari@outlook.com>
2023-12-01 13:23:23 +0100

    add: state storing pokemonIndex to App, and buttons for increase or decrease pokemonIndex, display current pokemon in list

--- pokedex/src/App.tsx
+++ pokedex/src/App.tsx
@@ -1,21 +1,49 @@
+import { useState } from 'react';
 import './App.css'
 import PokemonCard from './components/PokemonCard'
 
 function App() {
   const pokemonList = [
     {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
+        name: "bulbasaur",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+      },
+      {
+        name: "charmander",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+      },
+      {
+        name: "squirtle",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+      },
+      {
+        name: "pikachu",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+      },
+      {
+        name: "mew",
+      },
+    ];
+
+  const [pokemonIndex, setPokemonIndex] = useState(0)
+
+  const increasePokemonIndex = () => {
+    setPokemonIndex(pokemonIndex + 1)
+  }
+
+  const decreasePokemonIndex = () => {
+    setPokemonIndex(pokemonIndex - 1)
+  }
 
   return (
     <div>
-      <PokemonCard pokemon={pokemonList[1]}/>
+      <PokemonCard pokemon={pokemonList[pokemonIndex]}/>
+      {pokemonIndex > 0 ? <button onClick={decreasePokemonIndex}>Précédent</button> : null}
+      {pokemonIndex < pokemonList.length - 1 ? <button onClick={increasePokemonIndex}>Suivant</button> : null}
     </div>
   )
 }
